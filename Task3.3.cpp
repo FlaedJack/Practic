4b70207a603232e287894245bc58b227498fcbb7
FlaedJack <149298461+FlaedJack@users.noreply.github.com>
2024-06-11 11:20:36 +0300

    Create Task3.3.cpp

---
+++ Task3.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void d()
+{ 
+    // Стек вызовов: d() -> b() -> a() -> main()
+}
+void c()
+{
+}
+
+void b()
+{
+	c();
+	d();
+}
+
+void a()
+{
+	b();
+}
+
+int main()
+{
+	a();
+
+	return 0;
+}
