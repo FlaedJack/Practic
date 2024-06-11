e9a64914d1e20bb90e9d67c6fca47b1ce1ddc454
FlaedJack <149298461+FlaedJack@users.noreply.github.com>
2024-06-11 11:22:57 +0300

    Create Task4.4.cpp

---
+++ Task4.4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+void calculate(double height, int time) {
+    double heightNow;
+    int t = 0;
+    while (t <= time) {
+        heightNow = height - 0.5 * speedfall * (t * t);
+        if (heightNow > 0.0) {
+            cout << "Высота падения за " << t << " секунд " << heightNow << endl;
+        }
+        else {
+            cout << "Мяч на земле " << t << " секунд" << endl;
+            break;
+        }
+        t++;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    cout << "Введите высоту башни в метрах: ";
+    double height{};
+    cin >> height;
+
+    calculate(height, 5);
+
+    return 0;
+}
